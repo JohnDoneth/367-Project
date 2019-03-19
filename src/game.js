051bea50bf47806152dcd36ad5a951628d3fedee
John Doneth <doneth7@gmail.com>
2019-03-19 16:43:30 -0400

    Added resizing & FPS counter

--- src/game.js
+++ src/game.js
@@ -1,4 +1,11 @@
 var THREE = require("three");
+var stats = require("stats.js")();
+
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+
+document
+    .body
+    .appendChild(stats.dom);
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -17,12 +24,26 @@ scene.add(cube);
 
 camera.position.z = 5;
 
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
 function animate() {
+    stats.begin();
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+
+    stats.end();
 }
 animate();
