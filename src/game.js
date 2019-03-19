ef9229cbda94149a84f7e93052b3cfa919d88e0a
John Doneth <doneth7@gmail.com>
2019-03-19 17:16:26 -0400

    Trying to get shadows to work

--- src/game.js
+++ src/game.js
@@ -8,19 +8,37 @@ document
     .appendChild(stats.dom);
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default
 
 document
     .body
     .appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshPhongMaterial({color: 0x00ff00, specular: 0xffffff, reflectivity: 0.8, shininess: 1.0});
+// var geometry = new THREE.SphereBufferGeometry(1.0, 32, 16);
+var geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+
 var cube = new THREE.Mesh(geometry, material);
+cube.castShadow = true;
 scene.add(cube);
+
+var planeGeometry = new THREE.BoxGeometry(5, 1, 5);
+
+var plane = new THREE.Mesh(planeGeometry, material);
+plane.receiveShadow = true;
+plane.position.y -= 2.0;
+plane.rotation.x += Math.PI;
+scene.add(plane);
+
+var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+light.castShadow = true;
+
+scene.add(light);
 
 camera.position.z = 5;
 
