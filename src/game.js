6dc72b19d3bb4eb757072eef2ff2a4fe2c8496ef
John Doneth <doneth7@gmail.com>
2019-03-21 14:37:51 -0400

    Fixed shadows

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@ document
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default
@@ -19,28 +19,42 @@ document
     .body
     .appendChild(renderer.domElement);
 
-var material = new THREE.MeshPhongMaterial({color: 0x00ff00, specular: 0xffffff, reflectivity: 0.8, shininess: 1.0});
-// var geometry = new THREE.SphereBufferGeometry(1.0, 32, 16);
-var geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+var ambientLight = new THREE.AmbientLight( 0x707070 ); // soft white light
+scene.add( ambientLight );
 
-var cube = new THREE.Mesh(geometry, material);
+let light = new THREE.SpotLight( 0xffffff );
+//let light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
+light.castShadow = true;
+light.position.y = 5.0;
+scene.add(light);
+
+//Set up shadow properties for the light
+
+light.shadow.mapSize.width = 2048;  // default
+light.shadow.mapSize.height = 2048; // default
+light.shadow.camera.near = 0.5;       // default
+light.shadow.camera.far = 1000      // default
+light.shadow.radius = 1.5;
+light.shadow.bias = 0.0001;
+
+let material = new THREE.MeshPhongMaterial({color: 0xf0f0f0, specular: 0xffffff, reflectivity: 0.8, shininess: 1.0});
+// var geometry = new THREE.SphereBufferGeometry(1.0, 32, 16);
+let geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+
+let cube = new THREE.Mesh(geometry, material);
 cube.castShadow = true;
+cube.receiveShadow = true;
 scene.add(cube);
 
-var planeGeometry = new THREE.BoxGeometry(5, 1, 5);
+let planeGeometry = new THREE.BoxGeometry(5, 0.25, 5);
 
-var plane = new THREE.Mesh(planeGeometry, material);
+let plane = new THREE.Mesh(planeGeometry, material);
 plane.receiveShadow = true;
 plane.position.y -= 2.0;
 plane.rotation.x += Math.PI;
 scene.add(plane);
 
-var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
-light.castShadow = true;
-
-scene.add(light);
-
-camera.position.z = 5;
+camera.position.z = 8;
 
 window.addEventListener('resize', onWindowResize, false);
 
