3f1d176689eabbb384674db0be5fffe1b238a308
Justin King <personal.justinking@gmail.com>
2019-04-16 15:18:16 -0400

    Put in button for enabling/disabling shaders

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,35 @@ import Logger from "./util/Logger";
 
 class MazeGame {
   private _game: Game;
+  private _ballTrail : boolean;
 
   constructor() {
+    this._ballTrail = true;
     Logger.info("MazeGame v1.0.0 initialized.");
     document.getElementById("hint").style.display="none";
 
     const startScreen = document.getElementById("start-screen");
-    startScreen.getElementsByTagName("button")[0].addEventListener("click", () => {
+    document.getElementById("ballTrail").addEventListener("click", () =>{
+        this._ballTrail = !this._ballTrail;
+        if (this._ballTrail)
+          document.getElementById("ballTrail").innerHTML = "Ball trail enabled";
+        else
+          document.getElementById("ballTrail").innerHTML = "Ball trail disabled";
+    });
+    document.getElementById("playButton").addEventListener("click", () => {
       document.getElementById("start-screen").style.display = "none";
       document.getElementById("timer").style.display = "inline";
       document.getElementById("level").style.display="inline";
       document.getElementById("score").style.display="inline";
-      this._game = new Game(false, document.getElementById("ballTrail").checked);
+      this._game = new Game(false, this._ballTrail);
     });
-    startScreen.getElementsByTagName("button")[1].addEventListener("click", () => {
+    document.getElementById("nightMode").addEventListener("click", () => {
       document.getElementById("start-screen").style.display = "none";
       document.getElementById("timer").style.display = "inline";
       document.getElementById("level").style.display="inline";
       document.getElementById("hint").style.display="inline";
       document.getElementById("score").style.display="inline";
-      this._game = new Game(true, false);
+      this._game = new Game(true, this._ballTrail);
     });
   }
 }
